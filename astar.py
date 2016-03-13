1796734d52b7554f5a4362135d1cfc28bd3fd79b
leonjunwei <leon.lam.1994@gmail.com>
2016-03-13 19:15:21 -0400

    submitting AI toolbox!

--- astar.py
+++ astar.py
@@ -67,6 +67,15 @@ class GridWorld():
                 self.actors.pop(lava_coord, None)
         else:
             self.actors[lava_coord] = ObstacleTile( lava_coord, self, './images/lava.jpg', is_unpassable = True, terrain_cost = 0)
+    
+    def _add_swamp(self, mouse_pos):
+        swamp_coord = (mouse_pos[0]/50, mouse_pos[1]/50)
+        if self._is_occupied(swamp_coord):
+            if self.actors[swamp_coord].unremovable == False:
+                self.actors.pop(swamp_coord, None)
+        else:
+            self.actors[swamp_coord] = ObstacleTile( swamp_coord, self, './images/swamp.jpg', is_unpassable = False, terrain_cost = 3)
+
 
     def get_terrain_cost(self, cell_coord):
         try:
@@ -91,6 +100,8 @@ class GridWorld():
                 elif event.type is pygame.MOUSEBUTTONDOWN:
                     if self.add_tile_type == 'lava':
                         self._add_lava(event.pos)
+                    if self.add_tile_type == 'swamp':
+                    	self._add_swamp(event.pos)
                     #insert swamp code here
                 elif event.type is pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
@@ -98,6 +109,8 @@ class GridWorld():
                         self.paul.get_path()
                     elif event.key == pygame.K_l:
                         self.add_tile_type = 'lava'
+                    elif event.key == pygame.K_s:
+                    	self.add_tile_type = 'swamp'
                     #insert swamp code here
 
 class Actor(object):
@@ -143,9 +156,9 @@ class Cell():
 
     def draw(self):
         COST_TO_DRAW = ''
-        #COST_TO_DRAW = self.g_cost
-        #COST_TO_DRAW = self.h_cost
-        #COST_TO_DRAW = self.f_cost
+        # COST_TO_DRAW = self.g_cost #the cost of getting to the cell from the start
+        # COST_TO_DRAW = self.h_cost #how far the cell is from the goal, or the projected cost of getting to the goal from here
+        COST_TO_DRAW = self.f_cost #a measure of 'goodness' of a cell - the cost of getting there + its distance from the goal
         line_width = 2
         rect = pygame.Rect((self.coordinates[0],self.coordinates[1]),(self.dimensions[0],self.dimensions[1]))
         pygame.draw.rect(self.draw_screen, self.color, rect, line_width)
@@ -167,8 +180,8 @@ class Paul(Actor):
     def get_open_adj_coords(self, coords):
         """returns list of valid coords that are adjacent to the argument, open, and not in the closed list."""
         #modify directions and costs as needed
-        directions = [(1,0),(0,1),(-1,0),(0,-1)]
-        costs = [1,1,1,1]
+        directions = [(1,0),(0,1),(-1,0),(0,-1),(-1,-1),(1,1),(-1,1),(1,-1),(2,0),(0,2),(-2,0),(0,-2)]
+        costs = [1,1,1,1,3,3,3,3,8,8,8,8]
         adj_coords = map(lambda d: self.world._add_coords(coords,d), directions)
         for i, coord in enumerate(adj_coords):
             costs[i] += self.world.get_terrain_cost(coord)
